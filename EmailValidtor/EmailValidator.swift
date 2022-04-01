6a4785a0967bcddeb9d2de0871d2e01aed39677a
Vipin <sharmavipin11289@gmail.com>
2022-04-01 23:11:16 +0530

    latest

---
+++ EmailValidtor/EmailValidator.swift
@@ -0,0 +1,16 @@
+//
+//  EmailValidator.swift
+//  EmailValidtor
+//
+//  Created by Vipin on 01/04/22.
+//
+
+import Foundation
+
+public class EmailValidator {
+    public static func validateEmail(email:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+}
---
+++ EmailValidtor/EmailValidtor.h
@@ -0,0 +1,18 @@
+//
+//  EmailValidtor.h
+//  EmailValidtor
+//
+//  Created by Vipin on 01/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmailValidtor.
+FOUNDATION_EXPORT double EmailValidtorVersionNumber;
+
+//! Project version string for EmailValidtor.
+FOUNDATION_EXPORT const unsigned char EmailValidtorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmailValidtor/PublicHeader.h>
+
+
